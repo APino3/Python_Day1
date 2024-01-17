241e6e082edaa8e58113f955df08cb55bdcde4be
[Albalicia Espino] <Albaliciae@gmail.com>
2024-01-17 17:04:52 -0600

    added variables

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #Create a program that swaps the values of two variables.
+x = 15
+y = 30
+
+print("the values of x and y are {},{} before swapping".format(x,y))
+
+x, y = y, x
+print("the values of x and y are {},{} after swapping".format(x,y))
